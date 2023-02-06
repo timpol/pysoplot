48dd71680a6359e9478f48478ced1002abe0b250
timpol <109520354+timpol@users.noreply.github.com>
2023-02-06 13:15:57 +1100

    changed func names

--- src/pysoplot/useries.py
+++ src/pysoplot/useries.py
@@ -1,20 +1,6 @@
 """
 Uranium-series functions and routines.
 
-References
-----------
-.. [Cheng2000]
-    Cheng, H., Adkins, J., Edwards, R.L., Boyle, E.A., 2000. U-Th dating of
-    deep-sea corals. Geochimica et Cosmochimica Acta 64, 2401–2416.
-    https://doi.org/10.1016/S0016-7037(99)00422-6
-.. [Hellstrom2006]
-    Hellstrom, J., 2006. U–Th dating of speleothems with high initial 230Th using
-    stratigraphical constraint. Quaternary Geochronology 1, 289–295.
-    https://doi.org/10.1016/j.quageo.2007.01.004
-.. [Ludwig1977]
-    Ludwig, K.R., 1977. Effect of initial radioactive-daughter disequilibrium on
-    U-Pb isotope apparent ages of young minerals. Journal of Research of the US
-    Geological Survey 5, 663–667.
 """
 
 import numpy as np
@@ -28,36 +14,57 @@ exp = np.exp
 #=====================================
 # 234U age equaions
 #=====================================
-def ar48i(t, A48, l8, l4):
+#TODO: sec_eq should be a kwarg for these functions?, and dqpb functions should
+# set this to cfg.sec_eq.
+
+def ar48i(t, a234_238, lam238, lam234):
     """
-    Initial 234U/238U activity ratio from present ratio.
+    Calculate initial [234U/238U] activity ratio as a function of present
+    ratio and age (Ma).
 
     Notes
     ------
-    Non-secular equilibrium equation is derived following approach of Ludwig
-    (1977). This is an experimental feature only!
+    Non-secular equilibrium equation is derived from Bateman (1910) and does
+    not assume negligible decay of 238U (e.g., Ivanovich and Harmon
+    (1992)). This is an experimental feature.
+
+    References
+    ----------
+    Ivanovich, M. and Harmon, R. S.: Uranium-Series Disequilibrium:
+    Applications to Earth, Marine, and Environmental Sciences., Clarendon
+    Press, United Kingdom, second edn., 1992.
 
     """
     if cfg.secular_eq:
-        return 1. + (A48 - 1.) * exp(l4 * t)
+        return 1. + (a234_238 - 1.) * exp(lam234 * t)
     else:
-        return l4/(l4-l8) + (A48 - l4/(l4-l8)) *exp(-(l8-l4)*t)
+        return lam234/(lam234-lam238) + (a234_238 - lam234/(lam234-lam238)) \
+               * exp(-(lam238-lam234)*t)
 
 
-def ar48(t, A48i, l8, l4):
+def ar48(t, a234_238_i, lam238, lam234):
     """
-    234U/238U activity ratio from initial ratio and time.
+    Calculate initial [234U/238U] activity ratio as a function of initial
+    ratio and age (Ma).
 
     Notes
     ------
-    Non-secular equilibrium equation is derived following approach of Ludwig
-    (1977). This is an experimental feature only!
+    Non-secular equilibrium equation is derived from Bateman (1910) and does
+    not assume negligible decay of 238U (e.g., Ivanovich and Harmon
+    (1992)). This is an experimental feature.
+
+    References
+    ----------
+    Ivanovich, M. and Harmon, R. S.: Uranium-Series Disequilibrium:
+    Applications to Earth, Marine, and Environmental Sciences., Clarendon
+    Press, United Kingdom, second edn., 1992.
 
     """
     if cfg.secular_eq:
-        return (A48i - 1.) * exp(-l4 * t) + 1.
+        return 1. + (a234_238_i - 1.) * exp(-lam234 * t)
     else:
-        return l4/(l4-l8) + (A48i - l4/(l4-l8)) * exp((l8-l4)*t)
+        return lam234/(lam234-lam238) + (a234_238_i - lam234/(lam234-lam238)) \
+               * exp((lam238-lam234)*t)
 
 
 #=====================================
@@ -72,74 +79,100 @@ def Th230_age_uncert():
     pass
 
 
-def ar08i(t, A48, A08, l8, l4, l0, init=True):
+def ar08i(t, a234_238, a230_238, lam238, lam234, lam230, init=True):
     """
-    Initial [230Th/238U] as a function of present-day ratio and
-    time. E.g. Cheng et al. (2000; GCA) and Hellstrom (2006; Q. Geochon.).
+    Calculate initial [230Th/238U] as a function of present ratio and
+    time.
 
     Parameters
     ----------
     init : bool
-        If True, [234U/238] is treated as an initial value, otherwise it's
-        treated as a present-day value.
+        True if `a234_238` is as an initial activity ratio.
 
     Notes
     ------
-    Non-secular equilibrium equation is derived following approach of Ludwig
-    (1977). This is an experimental feature only!
+    Secular equilibrium equations are given by, e.g., Cheng et al., (2000) and
+    Hellstrom (2006).
+
+    Non-secular equilibrium equation is derived from Bateman (1910) and does
+    not assume negligible decay of 238U (e.g., Ivanovich and Harmon
+    (1992)). This is an experimental feature.
+
+    References
+    ----------
+    Cheng, H., Adkins, J., Edwards, R.L., Boyle, E.A., 2000.
+    U-Th dating of deep-sea corals. Geochimica et Cosmochimica Acta 64, 2401–2416.
+    https://doi.org/10.1016/S0016-7037(99)00422-6
+
+    Hellstrom, J., 2006. U–Th dating of speleothems with
+    high initial 230Th using stratigraphical constraint. Quaternary
+    Geochronology 1, 289–295. https://doi.org/10.1016/j.quageo.2007.01.004
 
     """
     if cfg.secular_eq:
         if init:
-            A48i = A48
-            A08i = 1. - (A48i-1.) * (l0/(l0-l4)) * (exp((l0-l4) * t) - 1) \
-                    + (A08-1.) * exp(l0*t)
+            a234_238_i = a234_238
+            a230_238_i = 1. - (a234_238_i - 1.) * (lam230/(lam230-lam234)) \
+                   * (exp((lam230-lam234) * t) - 1) + (a230_238 - 1.) * exp(lam230*t)
         else:
-            A08i = 1. - (A48-1.) * (l0/(l0-l4)) * (exp(l0*t) - exp(l4*t)) \
-                   + (A08-1.) * exp(l0 * t)
-        return A08i
+            a230_238_i = 1. - (a234_238 - 1.) * (lam230/(lam230-lam234)) \
+                   * (exp(lam230*t) - exp(lam234*t)) + (a230_238-1.) * exp(lam230 * t)
+        return a230_238_i
+
     else:
-        a48i = ar48i(t, A48, l8, l4) if init else A48
+        a234_238_i = ar48i(t, a234_238, lam238, lam234) if init else a234_238
 
-        c1 = l0*l4 / ((l4-l8) * (l0-l8))
-        c2 = l0*l4 / ((l8-l4) * (l0-l4))
-        c3 = l0*l4 / ((l8-l0) * (l4-l0))
+        c1 = lam230*lam234 / ((lam234-lam238) * (lam230-lam238))
+        c2 = lam230*lam234 / ((lam238-lam234) * (lam230-lam234))
+        c3 = lam230*lam234 / ((lam238-lam230) * (lam234-lam230))
 
-        A08i = (A08 - (c1 + c2*exp((l8-l4)*t) + c3*exp((l8-l0)*t)
-                + a48i*(l0/(l0-l4)) * (exp((l8-l4)*t) - exp((l8-l0) * t)))) \
-                * exp(-(l8-l0) * t)
+        A08i = (a230_238 - (c1 + c2*exp((lam238-lam234)*t) + c3*exp((lam238-lam230)*t)
+                + a234_238_i * (lam230/(lam230-lam234)) * (exp((lam238-lam234)*t)
+                   - exp((lam238-lam230) * t)))) * exp(-(lam238-lam230) * t)
         return A08i
 
 
-def A08(t, A48, A08i, l8, l4, l0, init=True):
+def ar08(t, a234_238, a230_238_i, lam238, lam234, lam230, init=True):
     """
-    Present-day [230Th/238U] activity ratio as a function of initial ratio
-    and time. Based on equations in, e.g., Cheng et al. (2000; GCA) and
-    Hellstrom (2006; Q. Geochon.).
+    Calculate present [230Th/238U] as a function of present ratio and
+    time.
 
     Parameters
     ----------
     init : bool
-        If True, [234U/238] is treated as an initial value, otherwise it's
-        treated as a present-day value.
+        True if `a234_238` is as an initial activity ratio.
 
     Notes
     ------
-    Non-secular equilibrium equation is derived following approach of Ludwig
-    (1977). This is an experimental feature only!
+    Secular equilibrium equations are given by, e.g., Cheng et al. (2000) and
+    Hellstrom (2006).
+
+    Non-secular equilibrium equation is derived from Bateman (1910) and does
+    not assume negligible decay of 238U (e.g., Ivanovich and Harmon
+    (1992)). This is an experimental feature.
+
+    References
+    ----------
+    Cheng, H., Adkins, J., Edwards, R.L., Boyle, E.A., 2000.
+    U-Th dating of deep-sea corals. Geochimica et Cosmochimica Acta 64, 2401–2416.
+    https://doi.org/10.1016/S0016-7037(99)00422-6
+
+    Hellstrom, J., 2006. U–Th dating of speleothems with
+    high initial 230Th using stratigraphical constraint. Quaternary
+    Geochronology 1, 289–295. https://doi.org/10.1016/j.quageo.2007.01.004
 
     """
     if not cfg.secular_eq:
         raise ValueError('non-secular equations not yet implemented here')
 
     if init:
-        A48i = A48
-        A08 = 1. - exp(-l0*t) * (1. - A08i) + (A48i-1.)*(l0/(l0-l4)) \
-              * (exp(-l4*t) - exp(-l0*t))
+        a234_238_i = a234_238
+        a230_238 = 1. - exp(-lam230*t) * (1. - a230_238_i) + (a234_238_i - 1.) \
+                    * (lam230/(lam230-lam234)) * (exp(-lam234*t) - exp(-lam230*t))
     else:
-        A08 = 1. - exp(-l0*t) * (1. - A08i) + (A48-1.) * (l0/(l0-l4)) \
-              * (1.-exp((l4-l0)*t))
-    return A08
+        a230_238 = 1. - exp(-lam230*t) * (1. - a230_238_i) + (a234_238-1.) \
+                   * (lam230/(lam230-lam234)) * (1.-exp((lam234-lam230)*t))
+    return a230_238
 
 
 #=====================================
@@ -155,19 +188,19 @@ def Th230_isochron():
 # Back-calculate initial activity ratio solutions
 #===================================================
 
-def ari_solutions(t, A, init, DC):
+def init_ratio_solutions(t, A, init, Lam):
     """
     Compute initial activity ratio solutions from present-day values and U-Pb age
     solution. In principle these values are computed iteratively along with the
-    age solution, but practically it is more convenient to compute them after
-    the fact.
+    age solution, but practically it is more convenient to compute them separately
+    
     """
-    A48i = None
-    A08i = None
+    a234_238_i = None
+    a230_238_i = None
     if not init[0]:        # present [234U/238U]
-        A48i = ar48i(t, A[0], DC[0], DC[1])
+        a234_238_i = ar48i(t, A[0], Lam[0], Lam[1])
         if not init[1]:    # present [234U/238U] and [230Th/238U]
-            A08i = ar08i(t, ar48i, A[1], DC[0], DC[1], DC[2])
+            a230_238_i = ar08i(t, ar48i, A[1], Lam[0], Lam[1], Lam[2])
     elif not init[1]:      # only present [230Th/238U]
-        A08i = ar08i(t, A[0], A[1], DC[0], DC[1], DC[2], init=init[0])
-    return A48i, A08i
+        a230_238_i = ar08i(t, A[0], A[1], Lam[0], Lam[1], Lam[2], init=init[0])
+    return a234_238_i, a230_238_i
