10a0bad7d75420c8371e7a73c98f43606a9b13be
timpol <109520354+timpol@users.noreply.github.com>
2023-02-06 13:24:33 +1100

    dosctrings, added two_sample_p

--- src/pysoplot/stats.py
+++ src/pysoplot/stats.py
@@ -1,20 +1,10 @@
 """
 Basic stats functions.
 
-References
-----------
-.. [Ludwig2012]
-    Ludwig, K.R., 2012. Isoplot/Ex Version 3.75: A Geochronological Toolkit for
-    Microsoft Excel, Special Publication 4. Berkeley Geochronology Center.
-.. [Wendt1991]
-    Wendt, I., Carl, C., 1991. The statistical distribution of the mean squared
-    weighted deviation. Chemical Geology: Isotope Geoscience section 86,
-    275–285. https://doi.org/10.1016/0168-9622(91)90010-T
-
 """
 
 import numpy as np
-from scipy import stats
+from scipy.stats import norm, t, chi2
 
 
 def nmad(x):
@@ -31,29 +21,48 @@ def t_critical(df, alpha=0.05):
     Critical Student's t value for df degrees of freedom.
     """
     q = 1. - alpha / 2
-    return stats.t.ppf(q, df)
+    return t.ppf(q, df)
+
+
+def two_sample_p(x, ox, y, oy):
+    """
+    Return p value for test that two measurements with **Gaussian** distributed
+    uncertainties and known sigma are the same. I.e. is x - y compatible with
+    0 given uncertainties?
+    """
+    d = abs(x - y)
+    sd = np.sqrt(ox ** 2 + oy ** 2)
+    p = 1. - norm.cdf(d, loc=0., scale=sd)
+    return p
 
 
 def pr_fit(df, mswd):
     """
-    Probability of fit following Ludwig (2012).
+    Compute probability of fit following Ludwig (2012).
+
+    References
+    ----------
+    Ludwig, K.R., 2012. Isoplot/Ex Version 3.75: A Geochronological Toolkit for
+    Microsoft Excel, Special Publication 4. Berkeley Geochronology Center.
+
     """
-    return 1. - stats.chi2.cdf(mswd * df, df)
+    return 1. - chi2.cdf(mswd * df, df)
 
 
 def mswd_conf_limits(df, p=0.95, two_tailed=False):
     """
-    Returns confidence limits on MSWD.
+    Returns confidence limits on MSWD for given df.
+
     """
     assert 0 < p < 1
     if two_tailed:
         p_low = (1. - p) / 2
         p_high = 1. - p_low
-        lower = stats.distributions.chi2.ppf(p_low, df) / df
-        upper = stats.distributions.chi2.ppf(p_high, df) / df
+        lower = chi2.ppf(p_low, df) / df
+        upper = chi2.ppf(p_high, df) / df
         return (lower, upper)
     else:
         # note: Wendt and Carl (1991) approximation is 1 + 2 * np.sqrt(2/df) ?
-        upper = stats.distributions.chi2.ppf(p, df) / df
+        upper = chi2.ppf(p, df) / df
         return upper
 
