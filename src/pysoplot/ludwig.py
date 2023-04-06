3eae6a1d9b903de4b86e9d41336a0a44b59530d3
timpol <109520354+timpol@users.noreply.github.com>
2023-04-06 13:32:45 +1000

    Make decay constant args in ludwig optional

--- src/pysoplot/ludwig.py
+++ src/pysoplot/ludwig.py
@@ -5,6 +5,7 @@ Disequilibrium U-Pb equations based on Ludwig (1977).
 
 import numpy as np
 
+from pysoplot import cfg
 from pysoplot import useries
 
 
@@ -13,7 +14,7 @@ log = np.log
 nan = np.nan
 
 
-def f(t, A, Lam, coef, init=(True, True)):
+def f(t, A, meas=(False, False), Lam=None, coef=None, comp=False):
     """
     206Pb*/238U ratio (where * denotes radiogenic Pb) as a function of t and
     activity ratio values following Ludwig (1977). Note there is a small typo
@@ -26,16 +27,16 @@ def f(t, A, Lam, coef, init=(True, True)):
     A : array-like
         One-dimensional array of activity ratio values arranged as follows
         [234U/238U], [230Th/238U], [226Ra/238U],
-    Lam : array-like
-        One-dimensional array of decay constants (Ma^-1) arragend as follows
-        [lam238, lam234, lam230, lam226]
-    coef : array-like
-        One-dimensional array of Bateman coefficients
-        [c1, c2, c3, c4, c5, p1, p2, p3, h1, h2]
-    init : array-like, optional
+    meas : array-like, optional
         Two element array of bools. First element is True if [234U/238U] is
-        an initial value. Second element is True if [230Th/238U] is an initial
-        value.
+        a present-day value. Second element is True if [230Th/238U] is a present-
+        day value.
+    Lam : array-like, optional
+        Decay constants ordered as [lam238, lam234, lam230, lam226].
+    coef : array-like, optional
+        Bateman coefficients correspaning to decay constants in Lam.
+    comp : bool, optional
+        Return 206Pb*/238U ratio as array of components.
 
     References
     -----------
@@ -44,24 +45,32 @@ def f(t, A, Lam, coef, init=(True, True)):
     the US Geological Survey 5, 663–667.
        
     """
+    if Lam is None:
+        Lam = (cfg.lam238, cfg.lam234, cfg.lam230, cfg.lam226)
+    if coef is None:
+        coef = bateman(Lam)
+
     # Get initial activity ratios if present value given.
-    a234_238_i = A[0] if init[0] else useries.ar48i(t, A[0], Lam[0], Lam[1])
-    a230_238_i = A[1] if init[1] else useries.ar08i(t, A[0], A[1], Lam[0], Lam[1], Lam[2], init=True)
+    a234_238_i = A[0] if not meas[0] else useries.aratio48i(t, A[0], Lam=Lam)
+    a230_238_i = A[1] if not meas[1] else useries.aratio08i(t, A[0], A[1], Lam=Lam, init=True)
     
     # Components of F from 238U and each intermediate daughter
     # ignoring 210Pb
-    F = exp(Lam[0] * t) * (coef[0] * exp(-Lam[0] * t) + coef[1] * exp(-Lam[1] * t)
+    F1 = exp(Lam[0] * t) * (coef[0] * exp(-Lam[0] * t) + coef[1] * exp(-Lam[1] * t)
             + coef[2] * exp(-Lam[2] * t) + coef[3] * exp(-Lam[3] * t) + 1.)
-    F += a234_238_i*Lam[0]/Lam[1]*exp(Lam[0]*t) * (coef[4] * exp(-Lam[1] * t)
+    F2 = a234_238_i*Lam[0]/Lam[1]*exp(Lam[0]*t) * (coef[4] * exp(-Lam[1] * t)
             + coef[5] * exp(-Lam[2] * t) + coef[6] * exp(-Lam[3] * t) + 1.)
-    F += a230_238_i*Lam[0]/Lam[2]*exp(Lam[0]*t) * (coef[7] * exp(-Lam[2] * t)
+    F3 = a230_238_i*Lam[0]/Lam[2]*exp(Lam[0]*t) * (coef[7] * exp(-Lam[2] * t)
             + coef[8] * exp(-Lam[3] * t) + 1.)
-    F += A[2]*Lam[0]/Lam[3]*exp(Lam[0]*t) * (1. - exp(-Lam[3] * t))
+    F4 = A[2]*Lam[0]/Lam[3]*exp(Lam[0]*t) * (1. - exp(-Lam[3] * t))
 
-    return F
+    if comp:
+        return F1, F2, F3, F4
+
+    return F1 + F2 + F3 + F4
 
 
-def g(t, a231_235_i, Lam, coef):
+def g(t, a231_235_i, Lam=None, coef=None, comp=False):
     """
     207Pb*/235U  ratio (where * denotes radiogenic Pb) as a function of t and
     activity ratio values following Ludwig (1977).
@@ -72,12 +81,14 @@ def g(t, a231_235_i, Lam, coef):
         Age (Ma)
     a231_235_i : float or array-like
         [231Pa/235U] activity ratio.
-    Lam : array-like
-        One-dimensional array of decay constants (Ma^-1) arragend as follows
+    Lam : array-like, optional
+        One-dimensional array of decay constants (Ma^-1) ordered as
         [lam235, lam231]
-    coef : array-like
-        One-dimensional array of Bateman coefficients
-        [d1, d2]
+    coef : array-like, optional
+        One-dimensional array of Bateman coefficients corresponding to decay
+        constants in Lam.
+    comp : bool, optional
+        Return 206Pb*/238U ratio as array of components.
 
     References
     -----------
@@ -86,42 +97,19 @@ def g(t, a231_235_i, Lam, coef):
     the US Geological Survey 5, 663–667.
 
     """
-    # ignoring 227Ac
-    G = exp(Lam[0] * t) * (coef[0] * exp(-Lam[0] * t) + coef[1] * exp(-Lam[1] * t) + 1.)
-    G += a231_235_i * Lam[0] / Lam[1] * exp(Lam[0] * t) * (1. - exp(-Lam[1] * t))
-    return G
+    if Lam is None:
+        Lam = (cfg.lam235, cfg.lam231)
+    if coef is None:
+        coef = bateman(Lam, series='235U')
 
-
-def f_comp(t, A, Lam, coef, init=(True, True)):
-    """
-    Return individual components of f.
-    """
-    # Get initial activity ratios if present value given.
-    a234_238_i = A[0] if init[0] else useries.ar48i(t, A[0], Lam[0], Lam[1])
-    a230_238_i = A[1] if init[1] else useries.ar08i(t, a234_238_i, A[1], Lam[0], Lam[1],
-                                              Lam[2], init=True)
-
-    # Components of F from 238U and each intermediate daughter
-        # ignoring 210Pb
-    F1 = exp(Lam[0] * t) * (coef[0] * exp(-Lam[0] * t) + coef[1] * exp(-Lam[1] * t)
-            + coef[2] * exp(-Lam[2] * t) + coef[3] * exp(-Lam[3] * t) + 1.)
-    F2 = a234_238_i*Lam[0]/Lam[1]*exp(Lam[0]*t) * (coef[4] * exp(-Lam[1] * t)
-            + coef[5] * exp(-Lam[2] * t) + coef[6] * exp(-Lam[3] * t) + 1.)
-    F3 = a230_238_i*Lam[0]/Lam[2]*exp(Lam[0]*t) * (coef[7] * exp(-Lam[2] * t)
-            + coef[8] * exp(-Lam[3] * t) + 1.)
-    F4 = A[2]*Lam[0]/Lam[3]*exp(Lam[0]*t) * (1. - exp(-Lam[3] * t))
-
-    return F1, F2, F3, F4
-
-
-def g_comp(t, a231_235_i, Lam, coef):
-    """
-    Return individual components of g.
-    """
     # ignoring 227Ac
     G1 = exp(Lam[0] * t) * (coef[0] * exp(-Lam[0] * t) + coef[1] * exp(-Lam[1] * t) + 1.)
     G2 = a231_235_i * Lam[0] / Lam[1] * exp(Lam[0] * t) * (1. - exp(-Lam[1] * t))
-    return G1, G2
+
+    if comp:
+        return G1, G2
+
+    return G1 + G2
 
 
 def bateman(Lam, series='238U'):
@@ -129,12 +117,12 @@ def bateman(Lam, series='238U'):
     Return Bateman coefficients for the 238U or 235U decay series as defined in
     Ludwig (1977).
 
+    Parameters
+    -----------
     Lam : array-like
         array of relevant decay constants (see below)
-
-    Notes
-    ------
-
+    series : {'238U', '235U'}, optional
+        Uranium series to compute coefficients for.
 
     References
     -----------
@@ -185,7 +173,7 @@ def bateman(Lam, series='238U'):
 # =================================
 
 
-def dfdt(t, A, Lam, coef, init=(True, True)):
+def dfdt(t, A, meas=(False, False), Lam=None, coef=None, comp=False):
     """
     Derivative of 206Pb*/238U ratio (where * denotes radiogenic Pb) with 
     respect to t.
@@ -196,6 +184,12 @@ def dfdt(t, A, Lam, coef, init=(True, True)):
     present.
 
     """
+
+    if Lam is None:
+        Lam = (cfg.lam238, cfg.lam234, cfg.lam230, cfg.lam226)
+    if coef is None:
+        coef = bateman(Lam)
+
     dF1 = Lam[0] * exp(Lam[0] * t) * (coef[0] * exp(-Lam[0] * t) 
             + coef[1] * exp(-Lam[1] * t) + coef[2] * exp(-Lam[2] * t) 
             + coef[3] * exp(-Lam[3] * t) + 1) + exp(Lam[0] * t) \
@@ -205,7 +199,7 @@ def dfdt(t, A, Lam, coef, init=(True, True)):
             - coef[3] * Lam[3] * exp(-Lam[3] * t))
 
     # initial [234U/238]
-    if init[0]:
+    if not meas[0]:
         dF2 = A[0] * Lam[0] / Lam[1] * (
                 coef[4] * (Lam[0] - Lam[1]) * exp((Lam[0] - Lam[1]) * t) 
                 + coef[5] * (Lam[0] - Lam[2]) * exp((Lam[0] - Lam[2]) * t)
@@ -226,7 +220,7 @@ def dfdt(t, A, Lam, coef, init=(True, True)):
                     + Lam[0] * exp(Lam[0] * t)))
 
     # initial [230Th/238U]
-    if init[1]:
+    if not meas[1]:
         dF3 = A[1] * Lam[0] / Lam[2] * (coef[7] * (
                 Lam[0] - Lam[2]) * exp((Lam[0] - Lam[2]) * t)
                 + coef[8] * (Lam[0] - Lam[3]) * exp((Lam[0] - Lam[3]) * t)
@@ -248,101 +242,32 @@ def dfdt(t, A, Lam, coef, init=(True, True)):
     dF4 = A[2]* Lam[0]/Lam[3] * (Lam[0] * exp(Lam[0]*t)
                 - (Lam[0]-Lam[3]) * exp((Lam[0]-Lam[3]) * t))
 
+    if comp:
+       return dF1, dF2, dF3, dF4
+
     return dF1 + dF2 + dF3 + dF4
 
 
-def dfdt_comp(t, A, Lam, coef, init=(True, True)):
-    """
-    Individual components of derivative of 206Pb*/238U ratio (where * denotes 
-    radiogenic Pb) with respect to t.
-    
-    Notes
-    ------
-    These equations are based on secular equilibrium U-series equations only at
-    present.
-    
-    """
-    dF1 = Lam[0] * exp(Lam[0] * t) * (
-            coef[0] * exp(-Lam[0] * t)
-            + coef[1] * exp(-Lam[1] * t) + coef[2] * exp(-Lam[2] * t)
-            + coef[3] * exp(-Lam[3] * t) + 1) + exp(Lam[0] * t) \
-           * (-coef[0] * Lam[0] * exp(-Lam[0] * t)
-              - coef[1] * Lam[1] * exp(-Lam[1] * t)
-              - coef[2] * Lam[2] * exp(-Lam[2] * t)
-              - coef[3] * Lam[3] * exp(-Lam[3] * t))
-
-    # initial [234U/238]
-    if init[0]:
-        dF2 = A[0] * Lam[0] / Lam[1] * (
-                coef[4] * (Lam[0] - Lam[1]) * exp((Lam[0] - Lam[1]) * t)
-                + coef[5] * (Lam[0] - Lam[2]) * exp((Lam[0] - Lam[2]) * t)
-                + coef[6] * (Lam[0] - Lam[3]) * exp((Lam[0] - Lam[3]) * t)
-                + Lam[0] * exp(Lam[0] * t))
-
-    # present [234U/238U]
-    else:
-        dF2 = Lam[1] * (A[0] - 1.) * exp(Lam[1] * t) * (
-                Lam[0] / Lam[1] * (
-                    coef[4] * exp((Lam[0] - Lam[1]) * t)
-                    + coef[5] * exp((Lam[0] - Lam[2]) * t)
-                    + coef[6] * exp((Lam[0] - Lam[3]) * t) + exp(Lam[0] * t))) \
-               + ((A[0] - 1.) * exp(Lam[1] * t) + 1.) * (
-                       Lam[0] / Lam[1] * (
-                       coef[4] * (Lam[0] - Lam[1]) * exp((Lam[0] - Lam[1]) * t)
-                       + coef[5] * (Lam[0] - Lam[2]) * exp((Lam[0] - Lam[2]) * t)
-                       + coef[6] * (Lam[0] - Lam[3]) * exp((Lam[0] - Lam[3]) * t)
-                       + Lam[0] * exp(Lam[0] * t)))
-
-    # initial [230Th/238U]
-    if init[1]:
-        dF3 = A[1] * Lam[0] / Lam[2] * (
-                coef[7] * (Lam[0] - Lam[2]) * exp((Lam[0] - Lam[2]) * t)
-                + coef[8] * (Lam[0] - Lam[3]) * exp((Lam[0] - Lam[3]) * t)
-                + Lam[0] * exp(Lam[0] * t))
-    # present [230Th/238U]
-    else:
-        dF3 = ((A[0] - 1.) * Lam[2] * exp(Lam[2] * t) - (A[0] - 1.) * (
-                Lam[2] / (Lam[2] - Lam[1])) * (Lam[2] - Lam[1])
-                * exp((Lam[2] - Lam[1]) * t)) \
-               * Lam[0] / Lam[2] * (coef[7] * exp((Lam[0] - Lam[2]) * t)
-                    + coef[8] * exp((Lam[0] - Lam[3]) * t) + exp(Lam[0] * t)) \
-               + ((A[0] - 1.) * exp(Lam[2] * t) - (A[0] - 1.)
-                  * (Lam[2] / (Lam[2] - Lam[1])) * (exp((Lam[2] - Lam[1]) * t)
-                    - 1.) + 1.) * Lam[0] / Lam[2] \
-               * (coef[7] * (Lam[0] - Lam[2]) * exp((Lam[0] - Lam[2]) * t)
-                  + coef[8] * (Lam[0] - Lam[3]) * exp((Lam[0] - Lam[3]) * t)
-                  + Lam[0] * exp(Lam[0] * t))
-
-    dF4 = A[2] * Lam[0] / Lam[3] * (Lam[0] * exp(Lam[0] * t)
-             - (Lam[0] - Lam[3]) * exp((Lam[0] - Lam[3]) * t))
-
-    return dF1, dF2, dF3, dF4
-
-
-def dgdt(t, a231_235_i, Lam, coef):
+def dgdt(t, a231_235_i, Lam=None, coef=None, comp=False):
     """
     Derivative of 207Pb*/235U (where * denotes radiogenic Pb) with respect to
     t.
     """
+    if Lam is None:
+        Lam = (cfg.lam235, cfg.lam231)
+    if coef is None:
+        coef = bateman(Lam, series='235U')
+
     dG1 = Lam[0] * exp(Lam[0] * t) * (coef[0] * exp(-Lam[0] * t)
             + coef[1] * exp(-Lam[1] * t) + 1.) + exp(Lam[0] * t) * (
             -coef[0] * Lam[0] * exp(-Lam[0] * t)
             - Lam[1] * coef[1] * exp(-Lam[1] * t))
     dG2 = a231_235_i * Lam[0] / Lam[1] * (Lam[0] * exp(Lam[0] * t)
             - (Lam[0]-Lam[1]) * exp((Lam[0]-Lam[1]) * t))
+
+    if comp:
+        return dG1, dG2
+
     return dG1 + dG2
 
-
-def dgdt_comp(t, a231_235_i, Lam, coef):
-    """
-    Individual components of derivative of 207Pb*/235U (where * denotes 
-    radiogenic Pb) with respect to t.
-    """
-    dG1 = Lam[0] * exp(Lam[0] * t) * (coef[0] * exp(-Lam[0] * t)
-            + coef[1] * exp(-Lam[1] * t) + 1.) + exp(Lam[0] * t) * (
-            -coef[0] * Lam[0] * exp(-Lam[0] * t)
-            - Lam[1] * coef[1] * exp(-Lam[1] * t))
-    dG2 = a231_235_i * Lam[0] / Lam[1] * (Lam[0] * exp(Lam[0] * t)
-            - (Lam[0]-Lam[1]) * exp((Lam[0]-Lam[1]) * t))
-    return dG1, dG2
 
